dbcf43a15512de9e5925dfed4fef2a5d74dba70a
xPanda <131697509+xPandaChristian@users.noreply.github.com>
2023-07-10 17:42:39 -0400

    NEW UPDATE!!!!!
    NEW UPDATE!!!!!

---
+++ code.js
@@ -0,0 +1,12 @@
+console.log("panda is cool")
+
+function button_click7() {
+    console.log("7")
+}
+
+function button_clickPlus() {
+    console.log("+")
+}
+function button_clickEquals() {
+    console.log("= + 14")
+}
